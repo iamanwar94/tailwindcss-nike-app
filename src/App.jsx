787e63f2394426ba1246af15d1f9dfc9756d2afe
anwar ahmed <muhammadanwar.symits@gmail.com>
2023-08-31 13:25:25 +0500

    nike app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { Nav } from "./components";
 import {
   CustomerReviews,
@@ -13,34 +13,34 @@ import {
 
 const App = () => {
   return (
-    <main className='relative'>
+    <main className="relative">
       <Nav />
-      <section className='xl:padding-l wide:padding-r padding-b'>
+      <section className="xl:padding-l wide:padding-r padding-b">
         <Hero />
       </section>
-      <section className='padding'>
+      <section className="padding">
         <PopularProducts />
       </section>
-      <section className='padding'>
+      <section className="padding">
         <SuperQuality />
       </section>
-      <section className='padding-x py-10'>
+      <section className="padding-x py-10">
         <Services />
       </section>
-      <section className='padding'>
+      <section className="padding">
         <SpecialOffer />
       </section>
-      <section className='bg-pale-blue padding'>
+      <section className="bg-pale-blue padding">
         <CustomerReviews />
       </section>
-      <section className='padding-x sm:py-32 py-16 w-full'>
+      <section className="padding-x sm:py-32 py-16 w-full">
         <Subscribe />
       </section>
-      <section className=' bg-black padding-x padding-t pb-8'>
+      <section className=" bg-black padding-x padding-t pb-8">
         <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
